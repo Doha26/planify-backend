69b8834998e8f93737be5dac358b637da3a7e589
Pavel FOUJEU <foujeupavel@gmail.com>
2025-01-15 01:56:19 +0100

    remove useless code + fix startup file path

--- startup.ci.sh
+++ startup.ci.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-./wait-for-it.sh postgres:5432
+/opt/wait-for-it.sh postgres:5432
 yarn run migration:run
 yarn run seed:run
 yarn run start:prod > prod.log 2>&1 &
-./wait-for-it.sh maildev:1080
-./wait-for-it.sh localhost:3000
+/opt/wait-for-it.sh maildev:1080
+/opt/wait-for-it.sh localhost:3000
 yarn run lint
 yarn run test:e2e -- --runInBand
--- startup.test.sh
+++ startup.test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-./wait-for-it.sh postgres:5432
-./wait-for-it.sh maildev:1080
+/opt/wait-for-it.sh postgres:5432
+/opt/wait-for-it.sh maildev:1080
 yarn install
 yarn run migration:run
 yarn run seed:run
